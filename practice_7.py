131bce66a9fb05fd27058f4806a38cc76dce1f7f
alan <ast9501@gmail.com>
2018-07-11 13:05:57 +0800

    program to find Prime number (between 1 ~ 50)with for loop

---
+++ practice_7.py
@@ -0,0 +1,9 @@
+#for loop 印 1 ~ 50 的質數
+for i in range(1, 51):
+    count = 0
+    for j in range(1, i+1):
+        if i % j == 0:
+            count += 1
+    if count == 2:
+        print(i)
+            
